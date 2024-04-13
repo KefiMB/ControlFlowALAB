ff3262b1eebf1ade4511b43c5b5cd0a7fb3acf2a
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-04-12 21:55:41 -0400

    Prediction

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("I did it!")
+
+// Part 1
+// Predicting the plant growth after a specific number of weeks
+let growth = 20 * 2 
+x = growth
+
+console.log(growth)
+// Implementing control flow to make decisions 
+
+
+
+
+
+
+
